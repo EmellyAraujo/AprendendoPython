5feb1119c8ce91e62f63a1c1d5fe093f8d39e502
Rhenanraiz1vs9 <pblucasbr@hotmail.com>
2019-02-21 08:32:27 -0300

    oiemyy

---
+++ MaximodeLIsta.py
@@ -0,0 +1,23 @@
+def main():
+  ''' teste da funcao maximo '''
+  # CRIE OS SEUS TESTES
+  #
+
+  minha_lista = [4,78,47,90,32,6,23]
+
+  maior, indice = max_lista(minha_lista)
+
+  print("O maior numero foi ", maior)
+  print("O indice = ", indice)
+
+
+def max_lista(lista):
+    ''' (list) -> int, int
+    recebe uma lista de inteiros e retorna o
+    valor maximo da lista e o indice onde ele ocorre '''
+
+    # ESCREVA A SUA FUNCAO
+
+    return 0, 0
+
+main()
